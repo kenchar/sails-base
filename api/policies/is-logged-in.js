0415bb4fa70387765b95f97f94005623a1d84832
kenchar <kenchar.lv@gmail.com>
2020-12-02 00:12:59 +0800

    登录策略新增token校验方式

--- api/policies/is-logged-in.js
+++ api/policies/is-logged-in.js
@@ -6,21 +6,44 @@
  */
 module.exports = async function (req, res, proceed) {
 
-  if(req.session
+  if (req.session
     && req.session.authenticated
-    && _.has(req.session,'passport')) return proceed();
-
-  if(_.isEmpty(req.headers.cookie)) return res.forbidden(new SessionError());
-
-  let sessionId = sails.session.parseSessionIdFromCookie(req.headers.cookie);
-
-  if(_.isEmpty(sessionId)) return res.forbidden(new SessionError());
-
-  sails.session.get(sessionId,function(err,session){
-    if(_.isNull(session) || _.isUndefined(session) || _.isUndefined(session.passport)) return res.forbidden(new SessionError());
-    req.session.passport = session.passport;
-    req.session.authenticated = true;
+    && _.has(req.session, 'passport')) {
     return proceed();
-  });
+  }
+
+  if (!_.isEmpty(req.headers.cookie)) {
+    //从cookie里获取登录会话信息
+    let sessionId = sails.session.parseSessionIdFromCookie(req.headers.cookie);
+
+    if (!_.isEmpty(sessionId)) {
+      sails.session.get(sessionId, function (err, session) {
+        if (_.isNull(session)
+          || _.isUndefined(session)
+          || _.isUndefined(session.passport)) {
+          return res.forbidden(new SessionError());
+        }
+        req.session.passport = session.passport;
+        req.session.authenticated = true;
+        return proceed();
+      });
+    } else {
+      return res.forbidden(new SessionError());
+    }
+  } else if (!_.isEmpty(req.headers.token) || !_.isEmpty(req.param('token'))) {
+    //从token里获取会话信息
+    let passport = await Passport.findOne({
+      accessToken: req.headers.token || req.param('token')
+    });
+    if (passport) {
+      req.session.passport = passport;
+      req.session.authenticated = true;
+      return proceed();
+    } else {
+      return res.forbidden(new SessionError());
+    }
+  } else {
+    return res.forbidden(new SessionError());
+  }
 
 };
